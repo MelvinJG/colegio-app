3b16d7ddd6fda9f096dfc2773f427c42ab7b9561
MelvinJG <melvinjoj58@gmail.com>
2022-10-06 06:40:56 -0600

    Migration to Angular SCSS

--- src/app/app.module_ORIGINAL.ts
+++ src/app/app.module_ORIGINAL.ts
@@ -4,23 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MdbFormsModule,
-    MdbDropdownModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
